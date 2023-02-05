7adb9f4659c86475ebf8d2a5c1fbb81c9819082b
Ricko <123415508+agony121@users.noreply.github.com>
2023-02-05 22:17:36 +0200

    Create manas.js

---
+++ manas.js
@@ -0,0 +1,7 @@
+ackage org.bitcoinj.wallettool;
+
+import org.bitcoinj.base.BitcoinNetwork;
+import org.bitcoinj.base.Sha256Hash;
+import org.bitcoinj.base.utils.ByteUtils;
+import org.bitcoinj.core.TransactionOutput;
+import org.bitcoinj.crypto.*;
